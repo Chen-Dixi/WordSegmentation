8562874f19d964955fba4d3ac9265394382a4f24
Chen-Dixi <cdxcqu2014@163.com>
2019-04-01 22:07:51 +0800

    分词脚本初步完成

--- segment.py
+++ segment.py
@@ -1,23 +1,10 @@
+#!/usr/local/bin/python2
+
 import codecs
 import sys
+
 import CRFPP
 
-def character_tagging(input_file, output_file,tagger):
-    input_data = codecs.open(input_file, 'r', 'utf-8')
-    output_data = codecs.open(output_file, 'w', 'utf-8')
-    for line in input_data.readlines():
-        word_list = line.strip().split()
-        for word in word_list:
-            if len(word) == 1:
-                output_data.write(word + "\tS\n")
-            else:
-                output_data.write(word[0] + "\tB\n")
-                for w in word[1:len(word)-1]:
-                    output_data.write(w + "\tM\n")
-                output_data.write(word[len(word)-1] + "\tE\n")
-        output_data.write("\n")
-    input_data.close()
-    output_data.close()
 
 def run_segment(input_file, output_file, tagger):
     """
@@ -33,7 +20,7 @@ def run_segment(input_file, output_file, tagger):
         tagger.clear()
         for word in line.strip():
             word = word.strip()
-            print(word)
+            
             if word:
                 tagger.add((word + "\tB\n").encode('utf-8'))
         tagger.parse()
@@ -46,7 +33,7 @@ def run_segment(input_file, output_file, tagger):
                 output_data.write(char)
                 if tag == 'E' or tag == 'S':
                     output_data.write(' ')
-        output_data.write("\n")# 每行话之间隔一行就好处理了
+        output_data.write("\n")
     input_data.close()
     output_data.close()
 
